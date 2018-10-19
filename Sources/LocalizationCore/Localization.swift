65dfafd5283b2a04a2f6f1fa9206719ad089fc05
Marek Fořt <marek.fort@ackee.cz>
2018-10-19 10:13:23 +0200

    Delete plist option.

--- Sources/LocalizationCore/Localization.swift
+++ Sources/LocalizationCore/Localization.swift
@@ -17,14 +17,13 @@ public final class Localization {
     
     public func run() throws {
         let main = command (
-            Flag("plist", description: "Use this flag if you wish to localize a plist file"),
             Argument<String>("google_doc_id", description: "ID of published spreadsheet"),
             Argument<String>("key_name", description: "Name of column which contains keys"),
             Argument<String>("key_mapping", description: "JSON of language mappings, e.g. {\"CZ\": \"cs\"}"),
             Argument<String>("output_dir", description: "Directory where output will be generated"),
             Argument<String>("strings_file_name", description: "Name of strings file, e.g. Localizable.strings")
             )
-        { isPlist, gdocID, lockKeyName, langMappingJSONString, outputDir, fileName in
+        { gdocID, lockKeyName, langMappingJSONString, outputDir, fileName in
             let langMapping = try JSONSerialization.jsonObject(with: langMappingJSONString.data(using: .utf8)!, options: .allowFragments) as! [String: String]
             
             let url = URL(string: "https://docs.google.com/spreadsheets/d/e/2PACX-" + gdocID + "/pub?output=tsv&time=" + String(Date().timeIntervalSince1970))!
@@ -48,28 +47,46 @@ public final class Localization {
             
             try values.forEach { key, values in
                 let outputArray = values.map { $0.localizableRow }
-                let output: String
-                if isPlist {
-                    let plistName = fileName.replacingOccurrences(of: ".strings", with: "")
-                    output = outputArray.filter { $0.contains("plist.") }.map { $0.replacingOccurrences(of: "plist." + plistName + ".", with: "", options: [.caseInsensitive]) }.joined(separator: "\n")
-                } else {
-                    output = outputArray.filter { !$0.contains("plist.") }.joined(separator: "\n")
+                let output = outputArray.filter { !$0.contains("plist.") }.joined(separator: "\n")
+                var plistOutputs: [String: String] = [:]
+
+                let plistFullStrings = outputArray.filter { $0.contains("plist.") }.map { $0.components(separatedBy: ".").dropFirst().joined(separator: ".") }
+                plistFullStrings.forEach {
+                    var components = $0.components(separatedBy: ".")
+                    let plistName = components.remove(at: components.startIndex) + ".strings"
+                    if var plist = plistOutputs[plistName] {
+                        plist += components.joined(separator: ".") + "\n"
+                    } else {
+                        plistOutputs[plistName] = components.joined(separator: ".") + "\n"
+                    }
                 }
 
-                let dir = NSString(string: outputDir).expandingTildeInPath + "/" + key + ".lproj"
-                let file = dir + "/" + fileName
-                
-                try? FileManager.default.removeItem(atPath: file)
-                try? FileManager.default.createDirectory(atPath: dir, withIntermediateDirectories: true, attributes: nil)
-                
                 do {
-                    try output.write(toFile: file, atomically: true, encoding: .utf8)
+                    try self.writeOutput(output, fileName: fileName, outputDir: outputDir, key: key)
                 } catch {
                     throw LocalizationError.unableToWrite
+                }
+
+                for (plistName, output) in plistOutputs {
+                    try? self.writeOutput(output, fileName: plistName, outputDir: outputDir, key: key)
                 }
             }
         }
         
         try main.run(Array(arguments.suffix(from: 1)))
+    }
+
+    private func writeOutput(_ output: String, fileName: String, outputDir: String, key: String) throws {
+        let dir = NSString(string: outputDir).expandingTildeInPath + "/" + key + ".lproj"
+        let file = dir + "/" + fileName
+
+        try? FileManager.default.removeItem(atPath: file)
+        try? FileManager.default.createDirectory(atPath: dir, withIntermediateDirectories: true, attributes: nil)
+
+        do {
+            try output.write(toFile: file, atomically: true, encoding: .utf8)
+        } catch {
+            throw LocalizationError.unableToWrite
+        }
     }
 }
