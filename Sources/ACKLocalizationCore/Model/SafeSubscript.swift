79a67070e6857572363494c9bfed332566f948f6
Jakub Olejník <jakub.olejnik@ackee.cz>
2019-12-12 22:02:18 +0100

    📝 Add documentation to safe subscript

--- Sources/ACKLocalizationCore/Model/SafeSubscript.swift
+++ Sources/ACKLocalizationCore/Model/SafeSubscript.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 internal extension RandomAccessCollection {
+    /// Returns element on `index` or `nil` if there isn't any
     subscript(safe index: Index) -> Element? {
         indices.contains(index) ? self[index] : nil
     }
