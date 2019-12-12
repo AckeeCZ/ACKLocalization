ff1ef8afb8c33c37cc46d86e6079c512e7693231
Jakub Olejník <jakub.olejnik@ackee.cz>
2019-12-12 21:54:59 +0100

    📝 Add documentation to `GoogleClaims`

--- Sources/ACKLocalizationCore/Model/GoogleClaims.swift
+++ Sources/ACKLocalizationCore/Model/GoogleClaims.swift
@@ -8,10 +8,20 @@
 import Foundation
 import SwiftJWT
 
+/// Struct that is used for generating second part of JWT token
 struct GoogleClaims: Claims {
+    /// Service account email
     let iss: String
+    
+    /// Required scope
     let scope = "https://www.googleapis.com/auth/spreadsheets.readonly"
+    
+    /// Desired auth endpoint
     let aud = "https://oauth2.googleapis.com/token"
+    
+    /// Date of expiration timestamp
     let exp: Int
+    
+    /// Issued at date timestamp
     let iat: Int
 }
