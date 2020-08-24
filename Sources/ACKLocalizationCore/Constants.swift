c072490084bc7a018f90dd091e4e1ffe66f8d526
Lukáš Hromadník <lukas.hromadnik@ackee.cz>
2020-08-24 10:44:56 +0200

    📝 Constants documentation

--- Sources/ACKLocalizationCore/Constants.swift
+++ Sources/ACKLocalizationCore/Constants.swift
@@ -12,7 +12,9 @@ public enum Constants {
     /// Prefix that defines that localization key holds plist item
     public static let plistKeyPrefix = "plist"
     
+    /// Regex pattern for plural keys
     public static let pluralKeyPattern = "(zero|one|two|few|many|other)"
     
+    /// Regex pattern for suffix of the plural translations
     public static let pluralPattern = #"\#\#\{\#(pluralKeyPattern)\}{1}$"#
 }
