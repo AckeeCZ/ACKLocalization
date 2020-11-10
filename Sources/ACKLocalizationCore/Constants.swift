ba196e64c1e6288f14c65faf70baab46a1897a96
Rostislav Babáček <babacek.r@gmail.com>
2020-11-10 17:53:48 +0100

    🔧 add sourcesKeyPrefix constant

--- Sources/ACKLocalizationCore/Constants.swift
+++ Sources/ACKLocalizationCore/Constants.swift
@@ -12,6 +12,9 @@ public enum Constants {
     /// Prefix that defines that localization key holds plist item
     public static let plistKeyPrefix = "plist"
     
+    /// Prefix that defines that localization key holds sources item
+    public static let sourcesKeyPrefix = "src"
+    
     /// Regex pattern for suffix of the plural translations
     public static let pluralPattern = #"^([\w.]+)?##\{([\w]+)?\}$"#
 }
