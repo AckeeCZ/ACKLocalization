14221868a7aee9b784a520c1de0de39320a3f22e
Rostislav Babáček <rostislav.babacek@ackee.cz>
2022-11-08 17:37:09 +0100

    ⬆️ add google auth dep

--- Package.swift
+++ Package.swift
@@ -19,13 +19,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url:"https://github.com/vapor/jwt-kit", .upToNextMajor(from: "4.3.0")),
+        .package(url: "https://github.com/googleapis/google-auth-library-swift", from: "0.5.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ACKLocalizationCore",
-            dependencies: ["JWTKit"]),
+            dependencies: ["JWTKit", "OAuth2"]),
         .target(
             name: "ACKLocalization",
             dependencies: ["ACKLocalizationCore"]),
