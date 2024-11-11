a5e0b72a4a2c3502d61ced34b4fdd5abef0e029a
Jakub Olejník <jakub.olejnik@ackee.cz>
2024-11-11 18:33:19 +0100

    Fix sorted keys in tests

--- Tests/ACKLocalizationCoreTests/ACKLocalization+Plurals.swift
+++ Tests/ACKLocalizationCoreTests/ACKLocalization+Plurals.swift
@@ -100,22 +100,22 @@ final class ACKLocalizationPluralsTests: XCTestCase {
         let rows = [
             LocRow(key: "key##{many}", value: "%d many"),
         ]
-        
-        let expectedResult: [String: Encodable] = [
-            "NSStringLocalizedFormatKey": "%#@inner@",
-            "inner": [
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .sortedKeys
+
+        let expectedResult = ExpectedPluralDict(
+            NSStringLocalizedFormatKey: "%#@inner@",
+            inner: [
                 "NSStringFormatSpecTypeKey": "NSStringPluralRuleType",
                 "NSStringFormatValueTypeKey": "d",
                 "many": "%d many"
             ]
-        ]
-        let expectedResultEncoded = try JSONSerialization
-            .data(withJSONObject: expectedResult, options: .sortedKeys)
+        )
+        let expectedResultEncoded = try encoder.encode(expectedResult)
         
         // When
         let plurals = try ackLocalization.buildPlurals(from: rows)
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .sortedKeys
+
         let encodedData = try encoder.encode(plurals.first?.value)
         
         // Then
@@ -127,25 +127,32 @@ final class ACKLocalizationPluralsTests: XCTestCase {
         let rows = [
             LocRow(key: "key##{many}", value: "%s many"),
         ]
-        
-        let expectedResult: [String: Encodable] = [
-            "NSStringLocalizedFormatKey": "%1$#@inner@",
-            "inner": [
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .sortedKeys
+
+        let expectedResult = ExpectedPluralDict(
+            NSStringLocalizedFormatKey: "%1$#@inner@",
+            inner: [
                 "NSStringFormatSpecTypeKey": "NSStringPluralRuleType",
                 "NSStringFormatValueTypeKey": "d",
                 "many": "%2$@ many"
             ]
-        ]
-        let expectedResultEncoded = try JSONSerialization
-            .data(withJSONObject: expectedResult, options: .sortedKeys)
-        
+        )
+        let expectedResultEncoded = try encoder.encode(expectedResult)
+
         // When
         let plurals = try ackLocalization.buildPlurals(from: rows)
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = .sortedKeys
         let encodedData = try encoder.encode(plurals.first?.value)
         
         // Then
         XCTAssertEqual(encodedData, expectedResultEncoded)
     }
+}
+
+// Well we used JSONSerialization for comparison of dict literal with expected Codable data,
+// but since Swift 6 it seems that JSONSerialization has inverse sorting for keys than JSONEncoder,
+// so we help ourselves this way
+private struct ExpectedPluralDict: Encodable {
+    let NSStringLocalizedFormatKey: String
+    let inner: [String: String]
 }
